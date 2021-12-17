1c7e63d96d78a3d6e5beaf4e5437858db1516ad5
Archibald Thirion <athirion@student.42.fr>
2021-12-17 10:54:07 +0100

    Fix installer

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ln -sv $HOME/Documents/config/.vimrc $HOME/.vimrc ln -sv $HOME/Documents/config/.zshrc $HOME/.zshrc
+ln -sv $HOME/Documents/config/.vimrc $HOME/.vimrc 
+ln -sv $HOME/Documents/config/.zshrc $HOME/.zshrc
 ln -sv $HOME/Documents/config/.tmux.conf $HOME/.tmux.conf
